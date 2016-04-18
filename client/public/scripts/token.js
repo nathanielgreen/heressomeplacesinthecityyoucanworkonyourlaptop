f12b123af07a94ca2db94c99d647cba0b5e27f80
nathanielgreen <ngreen2303@gmail.com>
2016-04-18 14:52:38 +0100

    mergin heroku

---
+++ client/public/scripts/token.js
@@ -0,0 +1,5 @@
+mapBoxAccount = {
+  attribution:'mapbox://styles/nathanielgreen/cil5cvlmd008qc1m585rvhumw',
+  id: 'nathanielgreen.o2pefhj4',
+  accessToken: 'pk.eyJ1IjoibmF0aGFuaWVsZ3JlZW4iLCJhIjoiY2lna3dmaXBvMDA3bHdlbTQ4bW05aHQ2ciJ9.PYKrjx24Ye5akPcNdbrwVw',
+};
